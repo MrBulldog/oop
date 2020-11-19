505ff4113ce1555c2cb828aa5b9e91d26571ecde
Mrbulldog <mrbulldog@Mrbulldogs-MacBook-Pro.local>
2020-11-19 13:48:41 +0700

    full

--- Main.py
+++ Main.py
@@ -16,3 +16,12 @@ class Employee:
         print("Nama :", self.nama)
         print("Gaji :", self.gaji)
         print()
+
+# Membuat objek pertama dari kelas Karyawan
+employee1 = Employee("Mimi", 5000000)
+# Membuat objek pertama dari kelas Karyawan
+employee2 = Employee("Yousef", 10000000)
+
+employee1.tampilkan_profile()
+employee2.tampilkan_profile()
+print("Total Karyawan: ", Employee.jumlah_karyawan)
